46dd85791f0fe4086af98d32e588e5875fd2f76a
SDW <stephenoilesanmi@gmail.com>
2023-03-04 14:48:00 +0000

    attempting to iterate through td elements for 1st row in table standings

--- scraper/main.py
+++ scraper/main.py
@@ -2,9 +2,11 @@ import requests
 from dotenv import load_dotenv
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+
 
 
 url = 'https://www.twtd.co.uk/league-tables/competition:premier-league/daterange/fromdate:2022-Jul-01/todate:2023-Jan-01/type:home-and-away/'
@@ -13,16 +15,20 @@ options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 chrome_driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
-# chrome_driver = webdriver.Chrome(options=options)
-
 try:
     chrome_driver.get(url)
-    chrome_driver.find_element(By.XPATH, '/html/body/div[8]/div[2]/div[1]/div[1]/button/i')
-    chrome_driver.click()
+    wait = WebDriverWait(chrome_driver, 5)
+    close_cookie_box = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[8]/div[2]/div[1]/div[1]/button/i')))
+    close_cookie_box.click()
+
+    
+    tr_element = chrome_driver.find_element(By.XPATH, '//*[@id="maincontent"]/table//*/tr[2]')
+    td_elements = chrome_driver.find_element(By.TAG_NAME, 'td')
+
+    for td in td_elements:
+        print(f'Content: {td}')
+
+    sleep(600)
+
 except Exception as e:
     print(e)
-
-
-
-
-
